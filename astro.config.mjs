5a2cb85c26f474def5cb9d769800e901e20cb2c5
MaxVanDijck <vandijcksm@gmail.com>
2024-03-27 08:59:16 +1300

    update astro-icons to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
