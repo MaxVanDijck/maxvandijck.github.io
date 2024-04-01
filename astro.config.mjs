dd0c6840e3d15c73b2d08af5f8700c0eb4afb39a
MaxVanDijck <vandijcksm@gmail.com>
2024-04-01 13:36:36 +1300

    almost ready to publish first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,19 @@ export default defineConfig({
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [rehypeKatex],
+    shikiConfig: {
+        // Choose from Shiki's built-in themes (or add your own)
+        // https://shiki.style/themes
+        theme: 'slack-dark',
+        // Add custom languages
+        // https://shiki.style/languages
+        langs: ["python", "diff"],
+        // Enable word wrap to prevent horizontal scrolling
+        wrap: false,
+        // Add custom transformers: https://shiki.style/guide/transformers
+        // Find common transformers: https://shiki.style/packages/transformers
+        transformers: [],
+    },
   }
 });
